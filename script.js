b810c077f3c972c42fa139697523b48df5ce4876
Đỗ Phương Nghi <130016300+Nxdhihi28@users.noreply.github.com>
2026-02-28 12:02:44 +0000

    commit

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function login(){
     let input = ""
     while(input != correct){
         input = prompt('Enter password:')
-        if (input != correct){
+        if (input != correct && input != ""){
             alert('Wrong password. Try again!')
         }
     }
@@ -97,3 +97,53 @@ productCards.forEach(function (card) {
         infoBox.innerHTML = "";
     });
 });
+
+// FIREBASE IMPORTS
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyA-GoWPNK94Hn2ENzjF8M9yUd4QH57HM0E",
+  authDomain: "dori-sheep-shop.firebaseapp.com",
+  projectId: "dori-sheep-shop",
+  storageBucket: "dori-sheep-shop.firebasestorage.app",
+  messagingSenderId: "137068014610",
+  appId: "1:137068014610:web:023983397cb15bbaa4fccb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// contact form
+const contact_form = document.getElementById("contact-form")
+
+contact_form.addEventListener("sudmit", async (e) => {
+    e.preventDefault();
+    
+    const contact_name = document.getElementById("contact-name").value
+    const contact_phone= document.getElementById("contact-phone").value
+    const contact_email = document.getElementById("contact-email").value
+    const contact_message = document.getElementById("contact-message").value
+
+    try{
+        await addDoc(collection(db, "contact"),{
+            name: contact_name,
+            phone: contact_phone,
+            email: contact_email,
+            message: contact_message,
+            createdAt: serverTimestamp()
+        })
+        alert("Message sent successfully!")
+        contact_form.reset()
+    }
+    catch (error){
+        console.error(" Error saving message: ",error);
+        alert("Something went wrong");
+    }
+})
+
+
+
+
